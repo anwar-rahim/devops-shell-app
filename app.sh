f02e46905edbd87546b479dc957595b6205e0e0f
anwar-rahim <anwarrahim1001@gmail.com>
2026-05-30 21:21:39 +0530

    update message 2

--- app.sh
+++ app.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 while true; do
-  echo -e "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nConnection: close\r\n\r\nDevOps Working from Shell Script" | nc -l 5000 -q 1
+  echo -e "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nConnection: close\r\n\r\nhahahahaah" | nc -l 5000 -q 1
 done
